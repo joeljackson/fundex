89b578f41ef7a46f8536b7a4d231d150e6624bea
Joel Jackson <jackson.joel@gmail.com>
2016-01-08 01:17:28 -0800

    Save values to firebase.

--- application.js
+++ application.js
@@ -1,20 +1,44 @@
-var app = angular.module('fundex', []);
+var app = angular.module('fundex', ["firebase"]);
 
 app.directive(
   'funCalendar',
-  function(){
+  function(memberService, weekService, funService){
     return {
       templateUrl: 'fun-calendar.html',
       restrict: 'A',
       controller: function() {
-        this.members = ['Joel'];
-        this.weeks = [1];
-
-        this.funFor = [0,7];
-//        this.funFor = function(developer, week) {
-//          return 7;
-//        }
+        this.members = memberService.members();
+        this.weeks = weekService.weeks();
+        this.funFor = funService.funs();
       },
       controllerAs: 'funController'
     }
   });
+
+app.service('memberService', function(){
+  this.members = function(){
+    return ['Joel'];
+  };
+
+  return this;
+});
+
+app.service('weekService', function(){
+  this.weeks = function(){
+    return [1];
+  };
+
+  return this;
+});
+
+app.service('funService', function(memberService, weekService, $firebaseObject){
+  this.funs = function(){
+    var funDb = new Firebase("https://fundex.firebaseio.com");
+
+    return _.object(_(memberService.members()).map(function(member){
+             return [member, $firebaseObject(funDb.child('funAmounts').child(member))];
+           }));
+  }
+
+  return this;
+});
